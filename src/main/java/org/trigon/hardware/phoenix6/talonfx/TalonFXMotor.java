b633bb02de46dea0f97ffdb34e08c24170e27d80
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2024-08-18 16:50:41 +0300

    Hehehe messy code

--- src/main/java/org/trigon/hardware/phoenix6/talonfx/TalonFXMotor.java
+++ src/main/java/org/trigon/hardware/phoenix6/talonfx/TalonFXMotor.java
@@ -4,6 +4,10 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.ControlRequest;
 import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import org.littletonrobotics.junction.Logger;
 import org.trigon.hardware.RobotHardwareStats;
 import org.trigon.hardware.phoenix6.Phoenix6Inputs;
@@ -26,7 +30,11 @@ public class TalonFXMotor {
         this.motorIO = generateIO(id, canbus);
         this.motorInputs = new Phoenix6Inputs(motorName);
         this.id = id;
-        motorIO.optimizeBusUsage();
+        Commands.run(this::dl);
+    }
+
+    private Command dl() {
+        return new WaitCommand(5).andThen(new InstantCommand(motorIO::optimizeBusUsage));
     }
 
     public void update() {
