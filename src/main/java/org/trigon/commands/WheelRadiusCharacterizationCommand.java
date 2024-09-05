919f8dfa03228896c51e2139c0f8022a1cdd77a1
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2024-09-05 22:44:31 +0300

    Fixed bug

--- src/main/java/org/trigon/commands/WheelRadiusCharacterizationCommand.java
+++ src/main/java/org/trigon/commands/WheelRadiusCharacterizationCommand.java
@@ -5,7 +5,7 @@
 // license that can be found in the LICENSE file at
 // the root directory of this project.
 
-package frc.trigon.robot.commands;
+package org.trigon.commands;
 
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Translation2d;
