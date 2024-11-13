dd22b53e99535a525cc87320a87337d1f4511fe5
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-11-13 20:04:45 +0200

    RobotHardwarsStats Changes (#17)
    * added new is sim logic
    
    * cleaned up code
    
    * I should look over what copilot writes for me more carefully

--- src/main/java/org/trigon/hardware/RobotHardwareStats.java
+++ src/main/java/org/trigon/hardware/RobotHardwareStats.java
@@ -5,9 +5,15 @@ public class RobotHardwareStats {
     private static boolean IS_REPLAY = false;
     private static double PERIODIC_TIME_SECONDS = 0.02;
 
-    public static void setCurrentRobotStats(boolean isReal, boolean isSimulation, boolean isReplay) {
-        IS_SIMULATION = isSimulation && !isReal;
-        IS_REPLAY = isReplay && !isReal;
+    public static void setCurrentRobotStats(boolean isReal, ReplayType replayType) {
+        if (isReal || replayType.equals(ReplayType.NONE)) {
+            IS_SIMULATION = !isReal;
+            IS_REPLAY = false;
+            return;
+        }
+        
+        IS_SIMULATION = replayType.equals(ReplayType.SIMULATION_REPLAY);
+        IS_REPLAY = true;
     }
 
     public static void setPeriodicTimeSeconds(double periodicTimeSeconds) {
@@ -24,5 +30,11 @@ public class RobotHardwareStats {
 
     public static boolean isSimulation() {
         return IS_SIMULATION;
+    }
+
+    public enum ReplayType {
+        NONE,
+        SIMULATION_REPLAY,
+        REAL_REPLAY
     }
 }
