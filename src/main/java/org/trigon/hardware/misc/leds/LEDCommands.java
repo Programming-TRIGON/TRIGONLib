ae3d972577ee07e1172b663f8b27c2c60b0f8743
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-11-15 12:03:11 +0200

    Forgot to do this earlier

--- src/main/java/org/trigon/hardware/misc/leds/LEDCommands.java
+++ src/main/java/org/trigon/hardware/misc/leds/LEDCommands.java
@@ -3,7 +3,7 @@ package org.trigon.hardware.misc.leds;
 import com.ctre.phoenix.led.LarsonAnimation;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -13,92 +13,85 @@ import java.util.function.Supplier;
  */
 public class LEDCommands {
     public static Command getStaticColorCommand(Color color, LEDStrip... LEDStrips) {
-        return new FunctionalCommand(
-                () -> runForLEDs((LEDStrip::clearLEDColors), LEDStrips),
+        return new StartEndCommand(
                 () -> {
+                    runForLEDs((LEDStrip::clearLEDColors), LEDStrips);
                     runForLEDs((LEDStrip -> LEDStrip.staticColor(color)), LEDStrips);
                     runForLEDs(LEDStrip -> LEDStrip.setCurrentAnimation(() -> LEDStrip.staticColor(color)), LEDStrips);
                 },
-                (interrupted) -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
-                () -> false,
+                () -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
                 LEDStrips
         ).ignoringDisable(true);
     }
 
-    public static Command getBlinkingCommand(Color firstColor, double speed, LEDStrip... LEDStrips) {
-        return new FunctionalCommand(
-                () -> runForLEDs((LEDStrip::clearLEDColors), LEDStrips),
+    public static Command getBlinkingCommand(Color firstColor, double blinkingIntervalSeconds, LEDStrip... LEDStrips) {
+        return new StartEndCommand(
                 () -> {
-                    runForLEDs((LEDStrip -> LEDStrip.blink(firstColor, speed)), LEDStrips);
-                    runForLEDs(LEDStrip -> LEDStrip.setCurrentAnimation(() -> LEDStrip.blink(firstColor, speed)), LEDStrips);
+                    runForLEDs((LEDStrip::clearLEDColors), LEDStrips);
+                    runForLEDs((LEDStrip -> LEDStrip.blink(firstColor, blinkingIntervalSeconds)), LEDStrips);
+                    runForLEDs(LEDStrip -> LEDStrip.setCurrentAnimation(() -> LEDStrip.blink(firstColor, blinkingIntervalSeconds)), LEDStrips);
                 },
-                (interrupted) -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
-                () -> false,
+                () -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
                 LEDStrips
         ).ignoringDisable(true);
     }
 
-    public static Command getBreatheCommand(Color color, int amountOfBreathingLEDs, double speed, boolean inverted, LarsonAnimation.BounceMode bounceMode, LEDStrip... LEDStrips) {
-        return new FunctionalCommand(
-                () -> runForLEDs((LEDStrip::clearLEDColors), LEDStrips),
+    public static Command getBreatheCommand(Color color, int amountOfBreathingLEDs, double cycleTimeSeconds, boolean inverted, LarsonAnimation.BounceMode bounceMode, LEDStrip... LEDStrips) {
+        return new StartEndCommand(
                 () -> {
-                    runForLEDs((LEDStrip -> LEDStrip.breathe(color, amountOfBreathingLEDs, speed, inverted, bounceMode)), LEDStrips);
-                    runForLEDs(LEDStrip -> LEDStrip.setCurrentAnimation(() -> LEDStrip.breathe(color, amountOfBreathingLEDs, speed, inverted, bounceMode)), LEDStrips);
+                    runForLEDs((LEDStrip::clearLEDColors), LEDStrips);
+                    runForLEDs((LEDStrip -> LEDStrip.breathe(color, amountOfBreathingLEDs, cycleTimeSeconds, inverted, bounceMode)), LEDStrips);
+                    runForLEDs(LEDStrip -> LEDStrip.setCurrentAnimation(() -> LEDStrip.breathe(color, amountOfBreathingLEDs, cycleTimeSeconds, inverted, bounceMode)), LEDStrips);
                 },
-                (interrupted) -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
-                () -> false,
+                () -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
                 LEDStrips
         ).ignoringDisable(true);
     }
 
-    public static Command getColorFlowCommand(Color color, double speed, boolean inverted, LEDStrip... LEDStrips) {
-        return new FunctionalCommand(
-                () -> runForLEDs((LEDStrip::clearLEDColors), LEDStrips),
+    public static Command getColorFlowCommand(Color color, double cycleTimeSeconds, boolean inverted, LEDStrip... LEDStrips) {
+        return new StartEndCommand(
                 () -> {
-                    runForLEDs((LEDStrip -> LEDStrip.colorFlow(color, speed, inverted)), LEDStrips);
-                    runForLEDs(LEDStrip -> LEDStrip.setCurrentAnimation(() -> LEDStrip.colorFlow(color, speed, inverted)), LEDStrips);
+                    runForLEDs((LEDStrip::clearLEDColors), LEDStrips);
+                    runForLEDs((LEDStrip -> LEDStrip.colorFlow(color, cycleTimeSeconds, inverted)), LEDStrips);
+                    runForLEDs(LEDStrip -> LEDStrip.setCurrentAnimation(() -> LEDStrip.colorFlow(color, cycleTimeSeconds, inverted)), LEDStrips);
                 },
-                (interrupted) -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
-                () -> false,
+                () -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
                 LEDStrips
         ).ignoringDisable(true);
     }
 
     public static Command getAlternateColorCommand(Color firstColor, Color secondColor, LEDStrip... LEDStrips) {
-        return new FunctionalCommand(
-                () -> runForLEDs((LEDStrip::clearLEDColors), LEDStrips),
+        return new StartEndCommand(
                 () -> {
+                    runForLEDs((LEDStrip::clearLEDColors), LEDStrips);
                     runForLEDs((LEDStrip -> LEDStrip.alternateColor(firstColor, secondColor)), LEDStrips);
                     runForLEDs(LEDStrip -> LEDStrip.setCurrentAnimation(() -> LEDStrip.alternateColor(firstColor, secondColor)), LEDStrips);
                 },
-                (interrupted) -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
-                () -> false,
+                () -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
                 LEDStrips
         ).ignoringDisable(true);
     }
 
     public static Command getSectionColorCommand(Supplier<Color>[] colors, LEDStrip... LEDStrips) {
-        return new FunctionalCommand(
-                () -> runForLEDs((LEDStrip::clearLEDColors), LEDStrips),
+        return new StartEndCommand(
                 () -> {
+                    runForLEDs((LEDStrip::clearLEDColors), LEDStrips);
                     runForLEDs((LEDStrip -> LEDStrip.sectionColor(colors)), LEDStrips);
                     runForLEDs(LEDStrip -> LEDStrip.setCurrentAnimation(() -> LEDStrip.sectionColor(colors)), LEDStrips);
                 },
-                (interrupted) -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
-                () -> false,
+                () -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
                 LEDStrips
         ).ignoringDisable(true);
     }
 
     public static Command getRainbowCommand(double brightness, double speed, LEDStrip... LEDStrips) {
-        return new FunctionalCommand(
-                () -> runForLEDs((LEDStrip::clearLEDColors), LEDStrips),
+        return new StartEndCommand(
                 () -> {
+                    runForLEDs((LEDStrip::clearLEDColors), LEDStrips);
                     runForLEDs((LEDStrip -> LEDStrip.rainbow(brightness, speed)), LEDStrips);
                     runForLEDs(LEDStrip -> LEDStrip.setCurrentAnimation(() -> LEDStrip.rainbow(brightness, speed)), LEDStrips);
                 },
-                (interrupted) -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
-                () -> false,
+                () -> runForLEDs(LEDStrip::clearLEDColors, LEDStrips),
                 LEDStrips
         ).ignoringDisable(true);
     }
