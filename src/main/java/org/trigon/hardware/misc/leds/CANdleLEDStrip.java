56be58673651ece1730e5567af8cde756155bf13
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-11-28 23:56:40 +0200

    LEDs (#16)
    * implemented AddressableLed and most of CANdleLed
    
    * finished CANdleLEDStrip and added LEDCommands
    
    * added colorFLowCommand
    
    * made code more efficient
    
    * cleaned
    
    * added jDocs for the patterns, and fixed colorFlow inverted
    
    * added requirements
    
    * made CANdleLEDStrip use AddressableLEDStrip in sim
    
    * cleaned a bunch
    
    * cleaned stuff
    
    * added jDocs and improved simulation
    
    * improved setLED method
    
    * cleaned up colorFlow
    
    * implementing this was soooooooooooooooooo annoying
    
    * made rainbow command work with params, and fixed var name
    
    * removed extra new line
    
    * fixed commands, and removed shouldLoop (We don't need it Ezra)
    
    * cleaned code and jDocs
    
    * reverted Command stuff, will figure out a solution tommorow
    
    * finally fixed critical error!!!!!!!!!!!!!!!!!!!!!!!! :8ball:
    
    * fixed CANdle LED sim bug and added more jdocs
    
    * jDoc changes, and replace intervalSeconds with speed. (CANdle animations are so annoying!
    
    * Forgot to do this earlier
    
    * cleaned a bit, optimized, and stuff
    
    * fixed method name and cleaned a bit
    
    * simple is good
    
    * minor optimization thing
    
    * cleaning. jdocs and rainbow inverted
    
    * sim cleaning thing. Still working on annoying LED_STRIPS array bug
    
    * cleaned a bit, finished sim logic and added initiateAddressableLED
    
    * cleaning

---
+++ src/main/java/org/trigon/hardware/misc/leds/CANdleLEDStrip.java
@@ -0,0 +1,159 @@
+package org.trigon.hardware.misc.leds;
+
+import com.ctre.phoenix.led.*;
+import edu.wpi.first.wpilibj.util.Color;
+import org.trigon.hardware.RobotHardwareStats;
+
+import java.util.function.Supplier;
+
+/**
+ * A LED strip that is controlled by a CANdle, and uses AddressableLED for simulation.
+ */
+public class CANdleLEDStrip extends LEDStrip {
+    private static CANdle CANDLE;
+    private static int LAST_CREATED_LED_STRIP_ANIMATION_SLOT = 0;
+    private final int animationSlot;
+
+    /**
+     * Sets the CANdle instance to be used for controlling the LED strips. Must be set before using any LED strips. Should only be called once
+     *
+     * @param candle the CANdle instance to be used
+     */
+    public static void setCANdle(CANdle candle) {
+        if (CANDLE == null)
+            CANDLE = candle;
+    }
+
+    /**
+     * Sets the total amount of LEDs in all LED strips for simulation.
+     * Must be set before using any LED strips in simulation. Should only be called once.
+     *
+     * @param totalAmountOfLEDs the total amount of LEDs in all LED strips
+     */
+    public static void setTotalAmountOfLEDs(int totalAmountOfLEDs) {
+        if (RobotHardwareStats.isSimulation() || RobotHardwareStats.isReplay())
+            AddressableLEDStrip.initiateAddressableLED(0, totalAmountOfLEDs);
+    }
+
+    /**
+     * Constructs a new CANdleLEDStrip. Before any commands are sent to the LED strip, the setCANdle method must be called.
+     *
+     * @param inverted     whether the LED strip is inverted
+     * @param numberOfLEDs the amount of LEDs in the strip
+     * @param indexOffset  the offset of the first LED in the strip
+     */
+    CANdleLEDStrip(boolean inverted, int numberOfLEDs, int indexOffset) {
+        super(inverted, numberOfLEDs, indexOffset);
+        animationSlot = LAST_CREATED_LED_STRIP_ANIMATION_SLOT;
+        LAST_CREATED_LED_STRIP_ANIMATION_SLOT++;
+    }
+
+    @Override
+    void clearLEDColors() {
+        CANDLE.clearAnimation(animationSlot);
+    }
+
+    @Override
+    void blink(Color firstColor, double speed) {
+        CANDLE.animate(
+                new SingleFadeAnimation(
+                        (int) firstColor.red,
+                        (int) firstColor.green,
+                        (int) firstColor.blue,
+                        0,
+                        speed,
+                        this.numberOfLEDs,
+                        indexOffset
+                ),
+                animationSlot
+        );
+    }
+
+    @Override
+    void staticColor(Color color) {
+        CANDLE.setLEDs((int) color.red, (int) color.green, (int) color.blue, 0, indexOffset, numberOfLEDs);
+    }
+
+    @Override
+    void breathe(Color color, int amountOfBreathingLEDs, double speed, boolean inverted, LarsonAnimation.BounceMode bounceMode) {
+        CANDLE.animate(
+                new LarsonAnimation(
+                        (int) color.red,
+                        (int) color.green,
+                        (int) color.blue,
+                        0,
+                        speed,
+                        this.numberOfLEDs,
+                        bounceMode,
+                        amountOfBreathingLEDs,
+                        indexOffset
+                ),
+                animationSlot
+        );
+    }
+
+    @Override
+    void alternateColor(Color firstColor, Color secondColor) {
+        for (int i = 0; i < numberOfLEDs; i++)
+            CANDLE.setLEDs(
+                    (int) (i % 2 == 0 ? firstColor.red : secondColor.red),
+                    (int) (i % 2 == 0 ? firstColor.green : secondColor.green),
+                    (int) (i % 2 == 0 ? firstColor.blue : secondColor.blue),
+                    0,
+                    i + indexOffset,
+                    1
+            );
+    }
+
+    @Override
+    void colorFlow(Color color, double speed, boolean inverted) {
+        final boolean correctedInverted = this.inverted != inverted;
+        CANDLE.animate(
+                new ColorFlowAnimation(
+                        (int) color.red,
+                        (int) color.green,
+                        (int) color.blue,
+                        0,
+                        speed,
+                        this.numberOfLEDs,
+                        correctedInverted ? ColorFlowAnimation.Direction.Backward : ColorFlowAnimation.Direction.Forward,
+                        indexOffset
+                ),
+                animationSlot
+        );
+    }
+
+    @Override
+    void rainbow(double brightness, double speed, boolean inverted) {
+        final boolean correctedInverted = this.inverted != inverted;
+        CANDLE.animate(
+                new RainbowAnimation(
+                        brightness,
+                        speed,
+                        this.numberOfLEDs,
+                        correctedInverted,
+                        indexOffset
+                ),
+                animationSlot
+        );
+    }
+
+    @Override
+    void sectionColor(Supplier<Color>[] colors) {
+        final int LEDSPerSection = (int) Math.floor(numberOfLEDs / colors.length);
+        setSectionColor(colors.length, LEDSPerSection, colors);
+    }
+
+    private void setSectionColor(int amountOfSections, int LEDSPerSection, Supplier<Color>[] colors) {
+        for (int i = 0; i < amountOfSections; i++) {
+            CANDLE.setLEDs(
+                    (int) (inverted ? colors[amountOfSections - i - 1].get().red : colors[i].get().red),
+                    (int) (inverted ? colors[amountOfSections - i - 1].get().green : colors[i].get().green),
+                    (int) (inverted ? colors[amountOfSections - i - 1].get().blue : colors[i].get().blue),
+                    0,
+                    LEDSPerSection * i + indexOffset,
+                    i == amountOfSections - 1 ? numberOfLEDs - 1 : LEDSPerSection * (i + 1) - 1
+            );
+        }
+    }
+}
---
+++ src/main/java/org/trigon/hardware/misc/leds/LEDStrip.java
@@ -0,0 +1,145 @@
+package org.trigon.hardware.misc.leds;
+
+import com.ctre.phoenix.led.LarsonAnimation;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.trigon.hardware.RobotHardwareStats;
+
+import java.util.function.Supplier;
+
+/**
+ * A wrapper class for LED strips. This class provides a set of methods for controlling LED strips.
+ */
+public abstract class LEDStrip extends SubsystemBase {
+    public static LEDStrip[] LED_STRIPS = new LEDStrip[0];
+    protected final int indexOffset;
+    protected final boolean inverted;
+    protected final int numberOfLEDs;
+    protected Runnable currentAnimation = () -> {
+    };
+
+    /**
+     * Creates a new AddressableLEDStrip.
+     *
+     * @param inverted     whether the LED strip is inverted
+     * @param numberOfLEDs the amount of LEDs in the strip
+     * @param indexOffset  the offset of the first LED in the strip
+     * @return the created AddressableLEDStrip
+     */
+    public static AddressableLEDStrip createAddressableLEDStrip(boolean inverted, int numberOfLEDs, int indexOffset) {
+        return new AddressableLEDStrip(inverted, numberOfLEDs, indexOffset);
+    }
+
+    /**
+     * Creates a new CANdleLEDStrip. In simulation or replay mode, an AddressableLEDStrip is created instead.
+     *
+     * @param inverted     whether the LED strip is inverted
+     * @param numberOfLEDs the amount of LEDs in the strip
+     * @param indexOffset  the offset of the first LED in the strip
+     * @return the created LEDStrip
+     */
+    public static LEDStrip createCANdleLEDStrip(boolean inverted, int numberOfLEDs, int indexOffset) {
+        if (RobotHardwareStats.isReplay() || RobotHardwareStats.isSimulation())
+            return new AddressableLEDStrip(inverted, numberOfLEDs, indexOffset);
+        return new CANdleLEDStrip(inverted, numberOfLEDs, indexOffset);
+    }
+
+    /**
+     * Sets the default command for all LED strips.
+     *
+     * @param command the default command to be set
+     */
+    public static void setDefaultCommandForAllLEDS(Command command) {
+        for (LEDStrip ledStrip : LED_STRIPS)
+            ledStrip.setDefaultCommand(command);
+    }
+
+    public LEDStrip(boolean inverted, int numberOfLEDs, int indexOffset) {
+        this.inverted = inverted;
+        this.numberOfLEDs = numberOfLEDs;
+        this.indexOffset = indexOffset;
+
+        addLEDStripToLEDStripsArray(this);
+    }
+
+    public int getNumberOfLEDS() {
+        return numberOfLEDs;
+    }
+
+    void setCurrentAnimation(Runnable currentAnimation) {
+        this.currentAnimation = currentAnimation;
+        currentAnimation.run();
+    }
+
+    void resetLEDSettings() {
+    }
+
+    /**
+     * Sets the color of the LED strip to the given color.
+     *
+     * @param color the color to set the LED strip to
+     */
+    abstract void staticColor(Color color);
+
+    /**
+     * Blinks the LED strip with a specific color.
+     *
+     * @param firstColor the color to blink
+     * @param speed      how fast the LED strip should blink on a scale between 0 and 1
+     */
+    abstract void blink(Color firstColor, double speed);
+
+    /**
+     * "Breathes" a pocket of LEDs with a given color.
+     *
+     * @param color         the color of the breathing LEDs
+     * @param breathingLEDs the amount of breathing LEDs
+     * @param speed         how fast should the color travel the strip on a scale between 0 and 1
+     * @param inverted      whether the breathing should be inverted
+     * @param bounceMode    when the breathing LEDs should restart at the start of the strip
+     */
+    abstract void breathe(Color color, int breathingLEDs, double speed, boolean inverted, LarsonAnimation.BounceMode bounceMode);
+
+    /**
+     * Flows a color through the LED strip.
+     *
+     * @param color    the color to flow through the LED strip
+     * @param speed    how fast should the color travel the strip on a scale between 0 and 1
+     * @param inverted whether the color flow should be inverted
+     */
+    abstract void colorFlow(Color color, double speed, boolean inverted);
+
+    /**
+     * Displays two colors in an alternating pattern on the LED strip.
+     *
+     * @param firstColor  the first color
+     * @param secondColor the second color
+     */
+    abstract void alternateColor(Color firstColor, Color secondColor);
+
+    /**
+     * Splits the LED strip into different sections.
+     *
+     * @param colors an array of the colors to color the sections with. The length of the array dictates the amount of sections
+     */
+    abstract void sectionColor(Supplier<Color>[] colors);
+
+    /**
+     * Displays a rainbow pattern on the LED strip.
+     *
+     * @param brightness the brightness of the rainbow on a scale from 0 to 1
+     * @param speed      the speed of the rainbow's movement on a scale from 0 to 1
+     * @param inverted   whether the rainbow should be inverted
+     */
+    abstract void rainbow(double brightness, double speed, boolean inverted);
+
+    abstract void clearLEDColors();
+
+    private void addLEDStripToLEDStripsArray(LEDStrip ledStrip) {
+        final LEDStrip[] newLEDStrips = new LEDStrip[LED_STRIPS.length + 1];
+        System.arraycopy(LED_STRIPS, 0, newLEDStrips, 0, LED_STRIPS.length);
+        newLEDStrips[LED_STRIPS.length] = ledStrip;
+        LED_STRIPS = newLEDStrips;
+    }
+}
