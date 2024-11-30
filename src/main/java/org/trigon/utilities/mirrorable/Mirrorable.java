8ed0e2027588140a7ba7ca976387e0c296500611
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-11-30 20:05:05 +0200

    idea for mirrorable fix

--- src/main/java/org/trigon/utilities/mirrorable/Mirrorable.java
+++ src/main/java/org/trigon/utilities/mirrorable/Mirrorable.java
@@ -25,10 +25,10 @@ public abstract class Mirrorable<T> {
     protected final T nonMirroredObject, mirroredObject;
     protected final boolean shouldMirrorWhenRedAlliance;
 
-    static {
-        UPDATE_ALLIANCE_TIMER.start();
-        new Trigger(() -> UPDATE_ALLIANCE_TIMER.advanceIfElapsed(0.5)).onTrue(getUpdateAllianceCommand());
-    }
+//    static {
+//        UPDATE_ALLIANCE_TIMER.start();
+//        new Trigger(() -> UPDATE_ALLIANCE_TIMER.advanceIfElapsed(0.5)).onTrue(getUpdateAllianceCommand());
+//    }
 
     /**
      * Creates a new mirrorable object.
