efb45e854c68da8e185bae3b353c2e9aa71dc524
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2025-01-10 00:51:43 +0200

    oops /

--- src/main/java/org/trigon/commands/NetworkTablesCommand.java
+++ src/main/java/org/trigon/commands/NetworkTablesCommand.java
@@ -31,7 +31,7 @@ public class NetworkTablesCommand extends Command {
         this.runPeriodically = runPeriodically;
         dashboardNumbers = new LoggedNetworkNumber[keys.length];
         for (int i = 0; i < keys.length; i++)
-            dashboardNumbers[i] = new LoggedNetworkNumber("/SmartDashboard/NetworkInputs" + keys[i]);
+            dashboardNumbers[i] = new LoggedNetworkNumber("/SmartDashboard/NetworkInputs/" + keys[i]);
         addRequirements(requirements.toArray(new Subsystem[0]));
     }
 
