db8790892caff40ed1dcaba9eda300adc38247e4
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2025-01-09 23:26:22 +0200

    false

--- src/main/java/org/trigon/hardware/grapple/lasercan/io/SimulationLaserCANIO.java
+++ src/main/java/org/trigon/hardware/grapple/lasercan/io/SimulationLaserCANIO.java
@@ -19,5 +19,6 @@ public class SimulationLaserCANIO extends LaserCANIO {
             return;
         inputs.distanceMillimeters = valueSupplier.getAsDouble();
         inputs.ambientLight = 0;
+        inputs.hasResult = false;
     }
 }
