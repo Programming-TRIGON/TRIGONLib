d3455e61fd1f6a63573a65b45b7b5f7020ced69a
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2025-01-15 16:43:03 +0200

    Added method to conversions

--- src/main/java/org/trigon/hardware/misc/servo/io/SimulationServoIO.java
+++ src/main/java/org/trigon/hardware/misc/servo/io/SimulationServoIO.java
@@ -4,10 +4,11 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.simulation.PWMSim;
 import org.trigon.hardware.misc.servo.ServoIO;
 import org.trigon.hardware.misc.servo.ServoInputsAutoLogged;
+import org.trigon.utilities.Conversions;
 
 public class SimulationServoIO extends ServoIO {
-    private static final double MAX_SERVO_ANGLE = 180;
-    private static final double MIN_SERVO_ANGLE = 0;
+    private static final double MAX_SERVO_ANGLE_DEGREES = 180;
+    private static final double MIN_SERVO_ANGLE_DEGREES = 0;
     private final PWMSim servoSim;
 
     public SimulationServoIO(int channel) {
@@ -17,7 +18,7 @@ public class SimulationServoIO extends ServoIO {
     @Override
     protected void updateInputs(ServoInputsAutoLogged inputs) {
         inputs.positionRotations = servoSim.getPosition();
-        inputs.targetAngle = Rotation2d.fromDegrees(servoSim.getPosition() * getServoAngleRange() + MIN_SERVO_ANGLE);
+        inputs.targetAngle = Rotation2d.fromDegrees(servoSim.getPosition() * getServoAngleRange() + MIN_SERVO_ANGLE_DEGREES);
         inputs.speed = servoSim.getSpeed();
     }
 
@@ -28,8 +29,8 @@ public class SimulationServoIO extends ServoIO {
 
     @Override
     protected void setAngle(Rotation2d angle) {
-        final double targetAngleDegrees = clampAngleToRange(angle);
-        setPosition((targetAngleDegrees - MIN_SERVO_ANGLE) / getServoAngleRange());
+        final double clampedAngleDegrees = Conversions.clampToRange(angle.getDegrees(), MIN_SERVO_ANGLE_DEGREES, MAX_SERVO_ANGLE_DEGREES);
+        setPosition((clampedAngleDegrees - MIN_SERVO_ANGLE_DEGREES) / getServoAngleRange());
     }
 
     @Override
@@ -37,16 +38,7 @@ public class SimulationServoIO extends ServoIO {
         servoSim.setPosition(position);
     }
 
-    private double clampAngleToRange(Rotation2d angle) {
-        double degrees = angle.getDegrees();
-        if (degrees < MIN_SERVO_ANGLE)
-            degrees = MIN_SERVO_ANGLE;
-        else if (degrees > MAX_SERVO_ANGLE)
-            degrees = MAX_SERVO_ANGLE;
-        return degrees;
-    }
-
     private double getServoAngleRange() {
-        return MAX_SERVO_ANGLE - MIN_SERVO_ANGLE;
+        return MAX_SERVO_ANGLE_DEGREES - MIN_SERVO_ANGLE_DEGREES;
     }
 }
