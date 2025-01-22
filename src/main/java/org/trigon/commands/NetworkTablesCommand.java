9c929b3ab55f647e71ab76eff33f89c09e18e1bb
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2025-01-22 21:48:14 +0200

    final

--- src/main/java/org/trigon/commands/NetworkTablesCommand.java
+++ src/main/java/org/trigon/commands/NetworkTablesCommand.java
@@ -120,7 +120,7 @@ public class NetworkTablesCommand extends Command {
     }
 
     private LoggedNetworkNumber[] generateDashboardNumbers(String... keys) {
-        LoggedNetworkNumber[] toReturn = new LoggedNetworkNumber[keys.length];
+        final LoggedNetworkNumber[] toReturn = new LoggedNetworkNumber[keys.length];
 
         for (int i = 0; i < keys.length; i++)
             toReturn[i] = new LoggedNetworkNumber("/SmartDashboard/" + keys[i], 0);
