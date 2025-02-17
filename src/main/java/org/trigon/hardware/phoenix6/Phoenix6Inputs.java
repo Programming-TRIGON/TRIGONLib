a997f006fb548c9a497d195f5a55a281fe43780c
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2025-02-17 19:49:10 +0200

    simple fix

--- src/main/java/org/trigon/hardware/phoenix6/Phoenix6Inputs.java
+++ src/main/java/org/trigon/hardware/phoenix6/Phoenix6Inputs.java
@@ -92,6 +92,8 @@ public class Phoenix6Inputs extends InputsBase {
     private void updateThreadedSignalsToTable(LogTable table) {
         for (Map.Entry<String, Queue<Double>> entry : signalToThreadedQueue.entrySet()) {
             final double[] queueAsArray = SignalThreadBase.queueToDoubleArray(entry.getValue());
+            if (queueAsArray.length == 0)
+                continue;
             table.put(entry.getKey() + "_Threaded", queueAsArray);
             table.put(entry.getKey(), queueAsArray[queueAsArray.length - 1]);
         }
