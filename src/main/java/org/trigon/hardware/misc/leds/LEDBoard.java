bb9fb70761391e1c9d18c6053e598e74a877476b
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2025-06-09 08:49:35 +0300

    CLean

--- src/main/java/org/trigon/hardware/misc/leds/LEDBoard.java
+++ src/main/java/org/trigon/hardware/misc/leds/LEDBoard.java
@@ -1,18 +1,19 @@
 package org.trigon.hardware.misc.leds;
 
-import com.ctre.phoenix.led.LarsonAnimation;
 import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.trigon.utilities.RGBArrayUtils;
 
 import java.io.IOException;
-import java.util.function.Supplier;
 
-public class LEDBoard extends SubsystemBase {
+public class LEDBoard {
     private final LEDStrip[] ledStrips;
 
     public LEDBoard(LEDStrip... ledStrips) {
         this.ledStrips = ledStrips;
+    }
+
+    public LEDStrip[] getLEDStrips() {
+        return ledStrips;
     }
 
     public void clearBoard() {
@@ -30,44 +31,5 @@ public class LEDBoard extends SubsystemBase {
                 ledStrips[i].setSingleLEDColor(j, currentColor);
             }
         }
-    }
-
-    public void staticColor(Color color) {
-        for (LEDStrip ledStrip : ledStrips)
-            ledStrip.staticColor(color);
-    }
-
-    public void blink(Color color, double speed) {
-        for (LEDStrip ledStrip : ledStrips)
-            ledStrip.blink(color, speed);
-    }
-
-    public void breathe(Color color, double speed, boolean inverted, LarsonAnimation.BounceMode bounceMode) {
-        for (LEDStrip ledStrip : ledStrips)
-            ledStrip.breathe(color, ledStrip.numberOfLEDs, speed, inverted, bounceMode);
-    }
-
-    public void colorFlow(Color color, double speed, boolean inverted) {
-        for (LEDStrip ledStrip : ledStrips)
-            ledStrip.colorFlow(color, speed, inverted);
-    }
-
-    public void alternateColor(Color firstColor, Color secondColor, boolean checkerBoard) {
-        for (int i = 0; i < ledStrips.length; i++) {
-            if (checkerBoard && i % 2 == 1)
-                ledStrips[i].alternateColor(secondColor, firstColor);
-            else
-                ledStrips[i].alternateColor(firstColor, secondColor);
-        }
-    }
-
-    public void sectionColor(Supplier<Color>[] colors) {
-        for (LEDStrip ledStrip : ledStrips)
-            ledStrip.sectionColor(colors);
-    }
-
-    public void rainbow(double brightness, double speed, boolean inverted) {
-        for (LEDStrip ledStrip : ledStrips)
-            ledStrip.rainbow(brightness, speed, inverted);
     }
 }
--- src/main/java/org/trigon/hardware/misc/leds/LEDCommands.java
+++ src/main/java/org/trigon/hardware/misc/leds/LEDCommands.java
@@ -172,7 +172,7 @@ public class LEDCommands {
                     }
                 },
                 ledBoard::clearBoard,
-                ledBoard
+                ledBoard.getLEDStrips()
         ).ignoringDisable(true).asProxy();
     }
 
