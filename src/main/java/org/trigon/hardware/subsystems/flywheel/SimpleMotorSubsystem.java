1eab76262d253187f1669a738d047d8d3eb8a3b3
ShmayaR <shmaya.rosenblatt@gmail.com>
2025-07-24 22:19:29 +0300

    added position (in degrees) to the update log

--- src/main/java/org/trigon/hardware/subsystems/flywheel/SimpleMotorSubsystem.java
+++ src/main/java/org/trigon/hardware/subsystems/flywheel/SimpleMotorSubsystem.java
@@ -68,7 +68,8 @@ public class SimpleMotorSubsystem {
     public void updateLog(SysIdRoutineLog log) {
         log.motor(name)
                 .angularVelocity(Units.RotationsPerSecond.of(motor.getSignal(TalonFXSignal.VELOCITY)))
-                .voltage(Units.Volts.of(motor.getSignal(TalonFXSignal.MOTOR_VOLTAGE)));
+                .voltage(Units.Volts.of(motor.getSignal(TalonFXSignal.MOTOR_VOLTAGE)))
+                .angularPosition(Units.Degrees.of(motor.getSignal(TalonFXSignal.POSITION)));
     }
 
     public void updateMechanism() {
