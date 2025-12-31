d171b24b0c23cc951ad28a19e9ced803e8e46db0
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2025-12-31 13:14:33 +0200

    Added TalonFXS support (#57)

---
+++ hardware/talonfxs/TalonFXSIO.java
@@ -0,0 +1,36 @@
+package frc.trigon.lib.hardware.talonfxs;
+
+import com.ctre.phoenix6.configs.TalonFXSConfiguration;
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.hardware.TalonFXS;
+import frc.trigon.lib.hardware.simulation.MotorPhysicsSimulation;
+
+public class TalonFXSIO {
+    protected void updateMotor() {
+    }
+
+    protected void setControl(ControlRequest request) {
+    }
+
+    protected void setPosition(double positionRotations) {
+    }
+
+    protected void applyConfiguration(TalonFXSConfiguration configuration) {
+    }
+
+    protected void optimizeBusUsage() {
+    }
+
+    protected void stopMotor() {
+    }
+
+    protected void setBrake(boolean brake) {
+    }
+
+    protected void setPhysicsSimulation(MotorPhysicsSimulation simulation) {
+    }
+
+    protected TalonFXS getTalonFXS() {
+        return null;
+    }
+}
